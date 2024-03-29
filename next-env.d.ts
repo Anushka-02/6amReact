1a4956805f8831fb397a138bcadbd052a3778e4a
Anushka-02 <47214815+Anushka-02@users.noreply.github.com>
2024-03-29 21:22:40 +0530

    Add files via upload

---
+++ next-env.d.ts
@@ -0,0 +1,16 @@
+{
+    "compilerOptions": {
+      "target": "esnext",
+      "module": "esnext",
+      "moduleResolution": "node",
+      "jsx": "preserve",
+      "esModuleInterop": true,
+      "strict": true,
+      "skipLibCheck": true,
+      "forceConsistentCasingInFileNames": true,
+      "resolveJsonModule": true,
+      "isolatedModules": true
+    },
+    "include": ["**/*.ts", "**/*.tsx"],
+    "exclude": ["node_modules"]
+  }
